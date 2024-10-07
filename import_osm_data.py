3560b8c0d48f78fa7beae24ad28d917bd46f5f58
basquiroto <fernandob.desouza@gmail.com>
2024-10-07 07:44:01 -0300

    First Commit.

---
+++ import_osm_data.py
@@ -0,0 +1,10 @@
+# %%
+import osmnx as ox
+import geopandas as gpd
+# %% 
+city_name = 'Criciúma, Santa Catarina, Brazil'
+graph = ox.graph_from_place(city_name, network_type='drive')
+nodes, edges = ox.graph_to_gdfs(graph)
+
+# %%
+print(edges.head())
